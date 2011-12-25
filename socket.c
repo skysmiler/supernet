29bcbd5150b901a4129e6a3db6f1ecfc77a98069
xiatianxiao <tianxiao.xtx@gmail.com>
2011-12-26 01:13:23 +0800

    子进程使用select，增加REUSEADDR选项

--- socket.c
+++ socket.c
@@ -21,16 +21,39 @@ void sys_quit( const char* );
 
 
 int _do_child( int sockfd ){
+    int res;
     fd_set allset, ioset;
+    char buf[128];
+    
+
     FD_ZERO( &allset );
     FD_SET( sockfd, &allset );
-
-    ioset = allset;
     
-//    select( sockfd+1, &ioset, NULL, NULL, NULL );
-    
-    write( sockfd, "world",5 );
-    
+    while( 1 ){
+        ioset = allset;
+        res = select( sockfd+1, &ioset, NULL, NULL, NULL );
+        if( res <=0 ){
+            perror( "child select:" );
+            continue;
+        }
+        if( FD_ISSET( sockfd, &ioset ) ){
+            memset( buf, 0, sizeof( buf ) );
+            res  = read( sockfd, buf, sizeof( buf ) );
+            if( res == 0 )
+                break;
+            else if( res >0 ){
+                if( buf[0] == '.' ){
+                    
+                    if( !strcmp( ".exit\n", buf ) )
+                        break;
+                }
+                
+                printf( "Read data:%d bytes\n", res );
+                write( sockfd, buf, res );
+            }
+            
+        }
+    }
 }
 int do_child( int sockfd ){
         
@@ -53,6 +76,10 @@ int main( int argc, char** argv ){
     fd_set rset;
     int    res;
     
+    if( argc !=2 )
+        return -1;
+    
+    
     listenfd = socket( AF_INET, SOCK_STREAM, 0 );
 
     memset( &serv_addr, 0, sizeof( serv_addr ) );
@@ -60,6 +87,10 @@ int main( int argc, char** argv ){
     serv_addr.sin_port   = htons( atoi( argv[1] ) );
     serv_addr.sin_addr.s_addr = INADDR_ANY;
     
+    res = 1;
+    setsockopt( listenfd, SOL_SOCKET, SO_REUSEADDR,&res,sizeof( int ) );
+    
+
     if(bind( listenfd, ( struct sockaddr* )&serv_addr, sizeof( serv_addr ) ) <0 )
         sys_error( "bind failed" );
     
@@ -81,10 +112,10 @@ int main( int argc, char** argv ){
             continue;
         }
         
-        
         if( FD_ISSET( listenfd, &rset ) ){
             if( ( connfd = accept( listenfd,NULL,NULL ) )<0 )
                 perror( "accept failed\n" );
+            
             if( fork() ==0 ){ /* child process */
                 close( listenfd );
                 do_child( connfd );
